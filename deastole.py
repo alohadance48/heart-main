7458d97576f28b6105efef59c7314e99d95c9441
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2024-11-22 01:23:46 +0300

    new version deastole.py

--- deastole.py
+++ deastole.py
@@ -1,34 +1,52 @@
-# deastole.py
 import time
-from info import Info
+
 
 class DestoleMain:
-    def __init__(self, deastole_start: bool):
+    def __init__(self, deastole_start: bool, defect_type: str):
         self.deastole_start = deastole_start
         self.blood_ejection_status = False
-        self.info_instance = Info()
+        self.defect_type = defect_type
+        self.pulse = 70  # Начальный пульс
 
     def deastole(self):
         self.blood_ejection_status = True
-        self.info_instance.deastole(self.blood_ejection_status)
+        self.adjust_pulse()
 
     def sistole(self):
         self.blood_ejection_status = False
-        self.info_instance.deastole(self.blood_ejection_status)
+        self.adjust_pulse()
+
+    def adjust_pulse(self):
+        if self.defect_type == "Heart Failure":
+            self.pulse = 60  # Если есть сердечная недостаточность, пульс замедляется
+        elif self.defect_type == "Diabetes":
+            self.pulse = 75  # Если диабет, пульс немного ускоряется
+        elif self.defect_type == "Ventricular Syndrome":
+            self.pulse = 80  # Синдром желудочков также ускоряет пульс
+        else:
+            self.pulse = 70  # Нормальный пульс без дефектов
 
     def get_blood_status(self):
-        return self.blood_ejection_status
+        return "Диастола" if self.blood_ejection_status else "Систола"
+
+    def get_pulse(self):
+        return self.pulse
+
 
 class Deastole(DestoleMain):
-    def main_start(self):
-        i = 0
-        while self.deastole_start:
-            i  += 1
-            if i  == 1:
-                time.sleep(2)
-                self.deastole()
-            else:
-                time.sleep(2)
-                self.sistole()
-                i = 0
+    def __init__(self, deastole_start: bool, defect_type: str):
+        super().__init__(deastole_start, defect_type)
+        self.time_interval = 1  # Устанавливаем значение по умолчанию для интервала
 
+    def set_time(self, time_interval):
+        self.time_interval = time_interval
+
+    def main_start(self, update_callback, update_pulse_callback):
+        while self.deastole_start:
+            time.sleep(self.time_interval)
+            if not self.blood_ejection_status:
+                self.deastole()
+                update_callback("Диастола")
+            else:
+                self.sistole()
+                update_callback("Систола")
