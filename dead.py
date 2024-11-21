e9d266fc11527030167ab6e3eb54d99f7b3822b7
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2024-11-22 01:23:15 +0300

    new version dead.py

--- dead.py
+++ dead.py
@@ -2,21 +2,14 @@ import time
 import threading
 from info import Info
 
-def timer_callback():
-    print('Вы умерли из-за перегрузки на сердце.')
-    Info().dead(True)
+def timer_callback(root: object):
+    root.result_label.configure(text="Вы погибли из-за перегрузки на сердце.")
 
-def heart_failure(fatigue: int, old: int, start: bool):
+def heart_failure(fatigue: int, old: int, root: object, start: bool):
     if fatigue >= 70 and old >= 55 and start:
-        print('У вас огромная нагрузка на сердце. Если в течение 10 секунд не сбросить нагрузку, вы умрете с шансом 70 %.')
-
-        timer = threading.Timer(10.0, timer_callback)
+        root.result_label.configure(text="У вас огромная нагрузка на сердце. Если в течение 10 секунд не сбросить нагрузку, вы умрете с шансом 70 %.")
+        timer = threading.Timer(10.0, timer_callback, args=(root,))
         timer.start()
 
-        for _ in range(10):
-            time.sleep(1)
-            if input('Для сброса нагрузки напиши "stop": ').lower() == 'stop':
-                timer.cancel()
-                print('Вы сбросили нагрузку!')
-                Info().dead(False)
-                return
+        return timer
+    return None
