e248f2e93a5b04730c274f5a5ff13f43aa911280
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2024-11-22 01:24:40 +0300

    new version  info.py

--- info.py
+++ info.py
@@ -1,10 +1,9 @@
-# info.py
 class Info:
     def deastole(self, status: bool) -> str:
         return f"Blood status is now {'Deastole' if status else 'Sistole'}"
 
     def user_info(self, is_old: int, fatigue: int):
-        return f'you old: {is_old}, твоя нагрузка: {fatigue}'
+        return f'Age: {is_old}, Fatigue: {fatigue}'
 
     def dead(self, start: bool):
-        return 'Вы погибли' if start else 'Вы выжили'
+        return 'You died' if start else 'You survived'
